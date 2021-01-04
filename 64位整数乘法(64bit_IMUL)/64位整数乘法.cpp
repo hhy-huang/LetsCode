397010abbfc3c869003e01de1035dad177b7394a
hhyer <68377817+hhy-huang@users.noreply.github.com>
2021-01-04 17:46:01 +0800

    Create 64位整数乘法.cpp

---
+++ 64位整数乘法(64bit_IMUL)/64位整数乘法.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+long long big_multi(long long a,long long b,long long m)
+{
+    long long ans = 0;
+    while(b)
+    {
+        if(b & 1)
+        {
+            ans = (ans + a) % m;//b的拆项
+        }
+        a = a * 2 % m;
+        b >>= 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    long long a,b,m;
+
+    cin>>a>>b>>m;
+    cout<<big_multi(a,b,m)<<endl;
+    return 0;
+}
