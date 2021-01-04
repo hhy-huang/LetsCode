89bb8da1c6f457676dc6e5f591369f2169478016
hhyer <68377817+hhy-huang@users.noreply.github.com>
2021-01-04 17:52:56 +0800

    Create 快速幂.cpp

---
+++ B数在心(快速幂_ Fast Modular Exponentiation)/快速幂.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+long long quick_pow(long long base,long long pow,long long m)
+{
+    long long ans = 1 % m;
+    while(pow)
+    {
+        if(pow & 1)
+        {
+            ans = ans * base % m;
+        }
+        base = base * base % m;//caculate the x^n
+        pow >>= 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    long long a,b,m;
+
+    cin>>a>>b>>m;
+    cout<<quick_pow(a,b,m)<<endl;
+    return 0;
+}
