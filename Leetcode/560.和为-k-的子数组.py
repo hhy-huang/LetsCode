11095c813db101df3b2623f5050533c9ab79d4b0
haoyuhuang <haoyuhuang@bjtu.edu.cn>
2024-04-25 21:28:22 +0800

    up

---
+++ Leetcode/560.和为-k-的子数组.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=560 lang=python3
+#
+# [560] 和为 K 的子数组
+#
+
+# @lc code=start
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        sums = 0
+        d = dict()
+        d[0] = 1
+        
+        for i in range(len(nums)):
+            sums += nums[i]
+            count += d.get(sums-k,0)
+            d[sums] = d.get(sums,0) + 1
+        
+        return count
+# @lc code=end
+
