7d8cc4275e531941fbef6c372a52e0f529bf8a49
haoyuhuang <haoyuhuang@bjtu.edu.cn>
2024-05-19 15:00:43 +0800

    up

--- Leetcode/200.number-of-islands.py
+++ Leetcode/200.number-of-islands.py
@@ -35,5 +35,27 @@ from typing import *
 
 # @leet start
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int: 
+    def dfs(self, grid, i, j, m, n):
+        if i < 0 or i >= m or j < 0 or j >= n:
+            return
+        if grid[i][j] == "2" or grid[i][j] == "0":
+            return
+
+        grid[i][j] = "2"
+        self.dfs(grid, i - 1, j, m, n)
+        self.dfs(grid, i + 1, j, m, n)
+        self.dfs(grid, i, j - 1, m, n)
+        self.dfs(grid, i, j + 1, m, n)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.res = 0
+        m = len(grid)
+        n = len(grid[0]) 
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    self.res += 1
+                    self.dfs(grid, i, j, m, n)
+        return self.res
+
 # @leet end
