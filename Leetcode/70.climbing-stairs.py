528963701452b96eaef28cf33c1e8978f7c2efdb
haoyuhuang <haoyuhuang@bjtu.edu.cn>
2024-05-23 21:41:32 +0800

    up

---
+++ Leetcode/70.climbing-stairs.py
@@ -0,0 +1,46 @@
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+
+# @leet start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1
+        b = 1
+        for i in range(n - 1):
+            temp = a
+            a = b
+            b = b + temp
+        return b
+# @leet end
